c284547f69e062ebc44d29f85b57c1b0d9f019fd
Esteban <esteban.fernando07@gmail.com>
2023-11-04 21:52:25 -0500

    bottom navbar basic

--- App.tsx
+++ App.tsx
@@ -1,14 +1,36 @@
 import React from 'react';
-import {StatusBar, StyleSheet, View} from 'react-native';
+import {StatusBar, StyleSheet, Text, View} from 'react-native';
 
 import {ScrollabelQuestionList} from './src/components/organisms/scrollable-questions-list/scrollable-question-list';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+
+const Tab = createBottomTabNavigator();
 
 const App = () => {
   return (
-    <View style={{flex: 1}}>
+    <NavigationContainer>
       <StatusBar barStyle={'light-content'} />
-      <ScrollabelQuestionList />
-    </View>
+      <Tab.Navigator
+        screenOptions={({route}) => ({
+          tabBarStyle: {backgroundColor: '#000'},
+          tabBarIcon: ({color, focused}) => {
+            let icon!: string;
+            switch (route.name) {
+              case 'Home':
+                icon = 'airplane-outline';
+                break;
+            }
+            return <Text>icon</Text>;
+          },
+        })}>
+        <Tab.Screen
+          name="Home"
+          options={{headerShown: false}}
+          component={ScrollabelQuestionList}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 };
 
--- android/app/src/main/java/com/teachtok/MainActivity.java
+++ android/app/src/main/java/com/teachtok/MainActivity.java
@@ -1,5 +1,7 @@
 package com.teachtok;
 
+import android.os.Bundle;
+
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
